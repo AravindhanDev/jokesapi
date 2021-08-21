585bb1833923e6b2c65d20e655b00e4be0cdfd53
NoobieUs37 <NoobieUs37@protonmail.com>
2021-08-21 14:15:15 +0530

    make mobile responsive

---
+++ build/precache-manifest.ca67da76227406c43f8bca0d0679b58a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2608fc7eadae32d5d81b0fa5174d482a",
+    "url": "/jokesapi/index.html"
+  },
+  {
+    "revision": "80b605e4d01f2ecffaf5",
+    "url": "/jokesapi/static/js/2.cb5930df.chunk.js"
+  },
+  {
+    "revision": "f41524e78fc3df27b86e",
+    "url": "/jokesapi/static/js/main.5832b287.chunk.js"
+  },
+  {
+    "revision": "bf6271eb4e9e078b1857",
+    "url": "/jokesapi/static/js/runtime-main.31e74e6d.js"
+  }
+]);
